8eeeaaf7c08b8576fe4453389f8411d2676b5b2e
sasin91 <jonas.kerwin.hansen@gmail.com>
2023-08-28 16:16:31 +0200

    Initial commit with i18next setup

---
+++ src/app/i18n/settings.ts
@@ -0,0 +1,17 @@
+export const fallbackLng = 'da'
+export const languages = [fallbackLng, 'en']
+export const defaultNS = 'translation'
+export const cookieName = 'i18next'
+
+export function getOptions (lng = fallbackLng, ns: string | string[] = defaultNS) {
+  return {
+    // debug: true,
+    supportedLngs: languages,
+    preload: languages,
+    fallbackLng,
+    lng,
+    fallbackNS: defaultNS,
+    defaultNS,
+    ns,
+  }
+}
